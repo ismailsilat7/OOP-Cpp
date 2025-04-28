d456805237a670bea2f0985d7db9a77fa302d4b5
Ismail Silat <ismailsilat7@gmail.com>
2025-04-28 09:31:30 +0000

    Converting string to numbers

---
+++ Lab 10/task3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+int main() {
+
+
+    ifstream file;
+
+    file.open("vehicles.txt");
+
+    if(!file.is_open()) {
+        cout << "Failed to open file"<< endl;
+        return 1;
+    }
+
+    string line;
+    while(getline(file,line)) {
+        if(!line.empty() && line[0] != '/') {
+
+            istringstream extract(line);
+            string Type, ID, Name, Year, extraData, Certification;
+            getline(extract, Type, ',');
+            getline(extract, ID, ',');
+            getline(extract, Name, ',');
+            getline(extract, Year, ',');
+            getline(extract, extraData, ',');
+            getline(extract, Certification, ',');
+
+            int intYear = stoi(Year);
+
+            cout << "Type: " <<Type << ", ID: " << ID << ", Name: " << Name << ", Year: " << intYear << ", Extra Data: " << extraData << ", Certification: " << Certification << endl;
+
+        }
+        
+    }
+
+    file.close();
+
+}
+
+
+
